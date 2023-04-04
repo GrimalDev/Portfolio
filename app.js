cbcd808484ea91fa5b6b396cd42bf4a8a9912dca
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-04-04 15:35:35 +0200

    better error handle
    if an article is not found, there is now a special page for it

--- app.js
+++ app.js
@@ -75,7 +75,13 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+
+  //if the error is a 404, render the 404 page
+  if (err.status === 404) {
+      res.render('not-found');
+  } else {
+      res.render('error');
+  }
 });
 
 app.listen(listeningPort, (err) => {
