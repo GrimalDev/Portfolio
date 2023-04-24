8f89621a018645acde0c2e171e3da19494de75ca
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-04-24 11:07:18 +0200

    add utf8mb4 charset support to the database

--- app/config/configDB.js
+++ app/config/configDB.js
@@ -10,7 +10,8 @@ const dbParams = {
     user    : process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port    : process.env.DB_PORT
+    port    : process.env.DB_PORT,
+    charset: 'utf8mb4'
 };
 
 const poolDB = mysql.createPool(dbParams);
